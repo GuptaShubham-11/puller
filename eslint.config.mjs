23dcb655524552edc4fdcde217351a7a32773bd3
GuptaShubham-11 <shu919847@gmail.com>
2026-07-26 21:32:01 +0530

    feat(format): add eslint and prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
   // Override default ignores of eslint-config-next.
+  {
+    rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'react/self-closing-comp': 'error',
+    },
+  },
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
